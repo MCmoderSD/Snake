1659f1e1beebc23382862ccd07f08cc77c61aeee
Seraphin Berger <MCmoderSD@web.de>
2024-10-27 01:25:55 +0200

    Updated to JsonUtility 1.1.0

--- src/main/java/de/MCmoderSD/main/Config.java
+++ src/main/java/de/MCmoderSD/main/Config.java
@@ -15,7 +15,11 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Config {
-
+    
+    // Constants
+    public static final String configPath = "/config/default.json";
+    public static final String streamingURL = "https://raw.githubusercontent.com/MCmoderSD/Snake/refs/heads/master/src/main/resources";
+    
     // Config Variables
     public static int SCALE;
     public static int FIELD_WIDTH;
@@ -86,19 +90,21 @@ public class Config {
         AudioLoader audioLoader = new AudioLoader();
         ImageLoader imageLoader = new ImageLoader();
         JsonUtility jsonUtility = new JsonUtility();
-        boolean isAbsolute = args.length > 1;
-
-        JsonNode config;
 
         // Load Config
-        String path = "/config/default.json";
-        config = jsonUtility.load(path, isAbsolute);
-        if (config == null)
-            config = jsonUtility.load("https://raw.githubusercontent.com/MCmoderSD/Snake/main/src/main/resources" + path);
+        JsonNode config = null;
+        boolean customConfig = args.length > 1;
+        boolean textureStreaming = !customConfig && Config.class.getResourceAsStream(configPath) == null;
 
-        // Null check
-        if (config == null) throw new IllegalArgumentException("The config file could not be loaded");
+        try {
+            config = customConfig ? jsonUtility.load(args[1], true) : jsonUtility.load(textureStreaming ? streamingURL + configPath : configPath, false);
+        } catch (IOException | URISyntaxException e) {
+            System.err.println("An error occurred while loading Config: " + e.getMessage());
+        }
 
+
+        // Check if config is null
+        if (config == null) throw new NullPointerException("Config is null");
 
         // Load Settings
         JsonNode settings = config.get("settings");
@@ -117,44 +123,80 @@ public class Config {
 
         // Load Assets
         JsonNode assets = config.get("assets");
+
+        // Load Images
+        JsonNode images = assets.get("images");
+        String iconPath = images.get("icon").asText();
+        String backgroundTilePath = images.get("backgroundTile").asText();
+        String backgroundCoverPath = images.get("backgroundCover").asText();
+        String headPath = images.get("head").asText();
+        String upperBodyPath = images.get("upperBody").asText();
+        String lowerBodyPath = images.get("lowerBody").asText();
+        String legTilePath = images.get("legTile").asText();
+        String legTransitionPath = images.get("legTransition").asText();
+        String feetPath = images.get("feet").asText();
+        String foodPath = images.get("food").asText();
+        String goldFoodPath = images.get("goldFood").asText();
+
+        // Load Animations
+        JsonNode animations = assets.get("animations");
+        String headAnimationPath = animations.get("head").asText();
+        String upperBodyAnimationPath = animations.get("upperBody").asText();
+        String lowerBodyAnimationPath = animations.get("lowerBody").asText();
+        String legTileAnimationPath = animations.get("legTile").asText();
+        String legTransitionAnimationPath = animations.get("legTransition").asText();
+        String feetAnimationPath = animations.get("feet").asText();
+        String opFoodAnimationPath = animations.get("opFood").asText();
+
+        // Load Sounds
+        JsonNode sounds = assets.get("sounds");
+        String foodSoundPath = sounds.get("food").asText();
+        String ultSoundPath = sounds.get("ult").asText();
+        String dieSoundPath = sounds.get("die").asText();
+
         try {
 
             // Load Images
-            JsonNode images = assets.get("images");
-            ICON = imageLoader.load(images.get("icon").asText());
-            BACKGROUND_TILE = scaleImage(imageLoader.load(images.get("backgroundTile").asText()));
-            BACKGROUND_COVER = scaleImage(imageLoader.load(images.get("backgroundCover").asText()));
-            HEAD = scaleImage(imageLoader.load(images.get("head").asText()));
-            UPPER_BODY = scaleImage(imageLoader.load(images.get("upperBody").asText()));
-            LOWER_BODY = scaleImage(imageLoader.load(images.get("lowerBody").asText()));
-            LEG_TILE = scaleImage(imageLoader.load(images.get("legTile").asText()));
-            LEG_TRANSITION = scaleImage(imageLoader.load(images.get("legTransition").asText()));
-            FEET = scaleImage(imageLoader.load(images.get("feet").asText()));
-            FOOD = scaleImage(imageLoader.load(images.get("food").asText()));
-            GOLD_FOOD = scaleImage(imageLoader.load(images.get("goldFood").asText()));
+            ICON = imageLoader.load(textureStreaming ? streamingURL + iconPath : iconPath);
+            BACKGROUND_TILE = scaleImage(imageLoader.load(textureStreaming ? streamingURL + backgroundTilePath : backgroundTilePath));
+            BACKGROUND_COVER = scaleImage(imageLoader.load(textureStreaming ? streamingURL + backgroundCoverPath : backgroundCoverPath));
+            HEAD = scaleImage(imageLoader.load(textureStreaming ? streamingURL + headPath : headPath));
+            UPPER_BODY = scaleImage(imageLoader.load(textureStreaming ? streamingURL + upperBodyPath : upperBodyPath));
+            LOWER_BODY = scaleImage(imageLoader.load(textureStreaming ? streamingURL + lowerBodyPath : lowerBodyPath));
+            LEG_TILE = scaleImage(imageLoader.load(textureStreaming ? streamingURL + legTilePath : legTilePath));
+            LEG_TRANSITION = scaleImage(imageLoader.load(textureStreaming ? streamingURL + legTransitionPath : legTransitionPath));
+            FEET = scaleImage(imageLoader.load(textureStreaming ? streamingURL + feetPath : feetPath));
+            FOOD = scaleImage(imageLoader.load(textureStreaming ? streamingURL + foodPath : foodPath));
+            GOLD_FOOD = scaleImage(imageLoader.load(textureStreaming ? streamingURL + goldFoodPath : goldFoodPath));
 
-            // Animations
-            JsonNode animations = assets.get("animations");
-            HEAD_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("head").asText())));
-            UPPER_BODY_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("upperBody").asText())));
-            LOWER_BODY_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("lowerBody").asText())));
-            LEG_TILE_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("legTile").asText())));
-            LEG_TRANSITION_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("legTransition").asText())));
-            FEET_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("feet").asText())));
-            OP_FOOD_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(animations.get("opFood").asText())));
+            // Load Animations
+            HEAD_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + headAnimationPath : headAnimationPath)));
+            UPPER_BODY_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + upperBodyAnimationPath : upperBodyAnimationPath)));
+            LOWER_BODY_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + lowerBodyAnimationPath : lowerBodyAnimationPath)));
+            LEG_TILE_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + legTileAnimationPath : legTileAnimationPath)));
+            LEG_TRANSITION_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + legTransitionAnimationPath : legTransitionAnimationPath)));
+            FEET_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + feetAnimationPath : feetAnimationPath)));
+            OP_FOOD_ANIMATION = new ImageIcon(scaleImage(imageLoader.load(textureStreaming ? streamingURL + opFoodAnimationPath : opFoodAnimationPath)));
 
             // Sounds
-            JsonNode sounds = assets.get("sounds");
-            FOOD_SOUND = audioLoader.load(sounds.get("food").asText());
-            ULT_SOUND = audioLoader.load(sounds.get("ult").asText());
-            DIE_SOUND = audioLoader.load(sounds.get("die").asText());
+            FOOD_SOUND = audioLoader.load(textureStreaming ? streamingURL + foodSoundPath : foodSoundPath);
+            ULT_SOUND = audioLoader.load(textureStreaming ? streamingURL + ultSoundPath : ultSoundPath);
+            DIE_SOUND = audioLoader.load(textureStreaming ? streamingURL + dieSoundPath : dieSoundPath);
         } catch (IOException | URISyntaxException e) {
             System.err.println("An error occurred while loading Images: " + e.getMessage());
         }
 
-        // Language set
-        LANGUAGE = args.length > 0 ? args[0] : "en";
-        JsonNode languageConfig = LANGUAGE.length() == 2 ? jsonUtility.load("/language/" + LANGUAGE + ".json", false) : jsonUtility.load(args[0], true);
+
+        // Load Language
+        JsonNode languageConfig;
+        try {
+            languageConfig = jsonUtility.load("/languages/en.json");
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Check if languageConfig is null
+        if (languageConfig == null) throw new NullPointerException("Language Config is null");
 
         // Language
         TITLE = languageConfig.get("title").asText();
